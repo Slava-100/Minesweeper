d2be549e69068b3f74c7f339291c99493844fbbb
Svyatoslav <svat.strel.2001@gmail.com>
2026-05-31 12:29:16 +0300

    Добавить логику заполнения карты + проверку

--- map.py
+++ map.py
@@ -1,91 +1,76 @@
-import cell
+from cell import Cell, TypeCell
 import random
 from enum import Enum
 
-class Сomplexity(Enum):
-    easy = 1,
-    medium = 2,
+
+class Complexity(Enum):
+    easy = (1,)
+    medium = (2,)
     hard = 3
-    
+
+
 class Map:
     def __init__(self, complexity):
-        if complexity == Сomplexity.easy:
-            self.size_x = 10
-            self.size_y = 10
-            self.count_bomb = 10
-        elif complexity == Сomplexity.medium:
-            self.size_x = 10
-            self.size_y = 10
-            self.count_bomb = 20
+        if complexity == Complexity.easy:
+            self.size_x, self.size_y, self.count_bomb = 10, 10, 10
+        elif complexity == Complexity.medium:
+            self.size_x, self.size_y, self.count_bomb = 10, 10, 20
         else:
-            self.size_x = 10
-            self.size_y = 10
-            self.count_bomb = 30
-            
-        self.cells = [self.size_x][self.size_y]
+            self.size_x, self.size_y, self.count_bomb = 10, 10, 30
+
+        self.cells = [[None for _ in range(self.size_x)] for _ in range(self.size_y)]
+        self.randomFillMap()
+        # self.FillCountBombOutside()
 
     def randomFillMap(self):
         for i in range(self.size_y):
             for j in range(self.size_x):
-                self.cells[i][j] = cell.Cell()
-    
+                self.cells[i][j] = Cell()
+
         current_count_bomb = 0
         while current_count_bomb < self.count_bomb:
-            index_x = random.randint(0, 19)
-            index_y = random.randint(0, 19)
-            if self.cells[index_x][index_y] == "B":
+            index_x = random.randint(0, self.size_x - 1)
+            index_y = random.randint(0, self.size_y - 1)
+            if self.cells[index_x][index_y].type == TypeCell.bomb:
                 continue
-            else:       
-                self.cells[index_x][index_y] = "B"
+            else:
+                self.cells[index_x][index_y].type = TypeCell.bomb
                 current_count_bomb += 1
 
-        
+    def FillCountBombOutside(self):
+        result = 0
+        for x in range(self.size_x):
+            for y in range(self.size_y):
+                if self.cells[x][y].type == TypeCell.bomb:
+                    continue
 
+                if x - 1 >= 0 and y - 1 >= 0:
+                    if self.cells[y - 1][x - 1].type == TypeCell.bomb:
+                        result += 1
+                if y - 1 >= 0:
+                    if self.cells[y - 1][x].type == TypeCell.bomb:
+                        result += 1
+                if x + 1 <= self.size_x - 1 and y - 1 >= 0:
+                    if self.cells[y - 1][x + 1].type == TypeCell.bomb:
+                        result += 1
+                if x - 1 >= 0 and y + 1 <= self.size_y - 1:
+                    if self.cells[y + 1][x - 1].type == TypeCell.bomb:
+                        result += 1
+                if y + 1 <= self.size_y - 1:
+                    if self.cells[y + 1][x].type == TypeCell.bomb:
+                        result += 1
+                if x + 1 <= self.size_x - 1 and y + 1 <= self.size_y - 1:
+                    if self.cells[y + 1][x + 1].type == TypeCell.bomb:
+                        result += 1
+                if x - 1 >= 0:
+                    if self.cells[y][x - 1].type == TypeCell.bomb:
+                        result += 1
+                if x + 1 <= self.size_x - 1:
+                    if self.cells[y][x + 1].type == TypeCell.bomb:
+                        result += 1
 
-def FillCountBombOutside(x, y, map):
-    result = 0
-    if x - 1 >= 0 and y - 1 >= 0:
-        if map[y - 1][x - 1] == "B":
-            result += 1
-    if y - 1 >= 0:
-        if map[y - 1][x] == "B":
-            result += 1
-    if x + 1 <= 19 and y - 1 >= 0:
-        if map[y - 1][x + 1] == "B":
-            result += 1
-    if x - 1 >= 0 and y + 1 <= 19:
-        if map[y + 1][x - 1] == "B":
-            result += 1
-    if y + 1 <= 19:
-        if map[y + 1][x] == "B":
-            result += 1
-    if x + 1 <= 19 and y + 1 <= 19:
-        if map[y + 1][x + 1] == "B":
-            result += 1
-    if x - 1 >= 0:
-        if map[y][x - 1] == "B":
-            result += 1
-    if x + 1 <= 19:
-        if map[y][x + 1] == "B":
-            result += 1
-    
-    map[y][x] = result                  
-
-
-
-for i in range(size_y):
-    for j in range(size_x):
-        print(map[i][j], end = ' ')
-    print()
-
-x = 1 
-while x != -1:    
-    x = int(input("x:"))
-    y = int(input("y:"))
-    map.FillCountBombOutside(x, y, map)
-
-    for i in range(size_y):
-        for j in range(size_x):
-            print(map[i][j], end = ' ')
-        print()
-        
+                if result == 0:
+                    self.cells[y][x].type = TypeCell.empty
+                else:
+                    self.cells[y][x].value = result
+                    self.cells[y][x].type = TypeCell.number
--- minesweeper.py
+++ minesweeper.py
@@ -1,7 +1,14 @@
-from map import Map
+from map import Map, Complexity
+from cell import TypeCell
 
-a = Map(map.Сomplexity.easy)
+game_map = Map(Complexity.easy)
 
-a.randomFillMap()
-
-print(a.cells)
+for row in game_map.cells:
+    for cell in row:
+        if cell.type == TypeCell.number:
+            print(cell.value, end=" ")
+        elif cell.type == TypeCell.bomb:
+            print("B", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
