4f7a83adf029a776a81c1c3872d07b27ddfc9896
SStrelnikov <SStrelnikov@SSTRELNIKOV>
2026-07-05 12:00:18 +0300

    поиграем

--- minesweeper.py
+++ minesweeper.py
@@ -1,14 +1,28 @@
 from map import Map, Complexity
 from cell import TypeCell
 
-game_map = Map(Complexity.easy)
+def showMap(game_map):
+    for row in game_map.cells:
+        for cell in row:
+            if cell.isOpen == True:
+                if cell.type == TypeCell.number:
+                    print(cell.value, end=" ")
+                else:
+                    print(" ", end=" ")
+            else:
+                print("*", end=" ")
+        print()
 
-for row in game_map.cells:
-    for cell in row:
-        if cell.type == TypeCell.number:
-            print(cell.value, end=" ")
-        elif cell.type == TypeCell.bomb:
-            print("B", end=" ")
-        else:
-            print(" ", end=" ")
-    print()
+def game(game_map):
+    end = False 
+    while end == False:
+        showMap(game_map)    
+        xy = input("Введи координаты хода (x y):")
+        xy_array = xy.strip().split()
+        x = int(xy_array[0])
+        y = int(xy_array[1])
+        game_map.play(x, y)       
+
+game_map = Map(Complexity.easy)
+game(game_map)
+
